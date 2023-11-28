23e94a2895164a81ceb87dab4fb4787058505442
Krishnakumar AP <krishnaap7@gmail.com>
2023-11-28 11:16:17 -0600

    Update read_preci.py

--- read_preci.py
+++ read_preci.py
@@ -6,43 +6,6 @@ Created on Sun Nov 26 14:52:09 2023
 @author: krishna
 """
 
-import pyart
-import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-import matplotlib as mpl  # Add this line to define mpl
-# Set global font size
-mpl.rcParams.update({'font.size': 20})  # Adjust the size as needed
-# Path to your NEXRAD radar file
-nexrad_file_path = '/nexrad/KHGX_SDUS34_N1PHGX_202207221910'
-# Read the radar file using Py-ART
-radar = pyart.io.read(nexrad_file_path)
-# Set up a map projection
-projection = ccrs.PlateCarree()
-# Create a plot
-fig = plt.figure(figsize=(10, 8))
-ax = plt.axes(projection=projection)
-
-# Plot the radar data
-display = pyart.graph.RadarMapDisplay(radar)
-display.plot_ppi_map('radar_estimated_rain_rate', 0, ax=ax, projection=projection,
-                     vmin=0, vmax=2, cmap='pyart_HomeyerRainbow')
-
-# Add coastlines, gridlines, etc.
-ax.coastlines('10m')
-ax.gridlines(draw_labels=True)
-
-# Mark a specific point with a star
-specific_lat = 29.90172
-specific_lon = -95.32496
-ax.plot(specific_lon, specific_lat, marker='*', color='black', markersize=14, transform=ccrs.Geodetic())
-# Customize gridlines
-gl = ax.gridlines(draw_labels=True)
-gl.top_labels = False  # Disable top x-axis labels
-gl.right_labels = False  # Disable right y-axis labels
-
-# Show the plot
-plt.title('NEXRAD Radar Estimated Rain Rate')
-plt.show()
 
 #%%
 import matplotlib.pyplot as plt
@@ -70,11 +33,6 @@ ax.coastlines('10m')
 gl = ax.gridlines(draw_labels=True)
 gl.top_labels = False  # Disable top x-axis labels
 gl.right_labels = False  # Disable right y-axis labels
-
-# Mark a specific point with a star
-specific_lat = 29.90172
-specific_lon = -95.32496
-ax.plot(specific_lon, specific_lat, marker='*', color='black', markersize=20, transform=ccrs.Geodetic())  # Adjust marker size as needed
 
 # Show the plot with larger title
 plt.title('NEXRAD Radar Estimated Rain Rate with Marked Star', fontsize=16)  # Adjust title font size as needed
